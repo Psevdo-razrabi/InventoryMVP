c4dd4ed5ba7924e0a931669821a42704d5513f04
KOBAN13 <layk.danil@mail.ru>
2024-09-26 20:15:36 +0400

    добавил SO для хранения данных инвентаря + формирование ID

---
+++ Assets/Scripts/ConfigsScripts/ItemDescription.cs
@@ -0,0 +1,13 @@
+using System;
+using CustomObserverable;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Item", menuName = "Inventory/Item")]
+public class ItemDescription : ScriptableObject
+{
+    [field: SerializeField] public string NameItem { get; private set; }
+    [field: SerializeField] public int MaxStack { get; private set; }
+    [field: SerializeField] public Sprite Sprite { get; private set; }
+    [field: SerializeField] public string Description { get; private set; }
+    public GuidItem Id { get; private set; } = GuidItem.NewGuid();
+}
